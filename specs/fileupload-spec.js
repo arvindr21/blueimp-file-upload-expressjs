965392673a29518059aaae9bd321a62282ba7d6c
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2014-05-01 12:15:57 +0530

    Added Tests

---
+++ specs/fileupload-spec.js
@@ -0,0 +1,9 @@
+var fileUpload = require("../index");
+
+describe("dummy-test", function () {
+  var hello = "Hello";
+  it("says hello", function () {
+    var greetings = "Hello";
+    expect(greetings).toBe(hello);
+  });
+ });
